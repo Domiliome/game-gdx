180e6e900048b69bceb3b7cec7d6ee2abaa5682e
Evgeny Reish <e1000057r@gmail.com>
2026-07-24 14:50:59 +0300

    fix zoom

--- core/src/main/java/io/github/simple_game/core/service/InteractionService.java
+++ core/src/main/java/io/github/simple_game/core/service/InteractionService.java
@@ -24,9 +24,9 @@ public class InteractionService implements GestureDetector.GestureListener {
 
     private float initialZoom = 1.0f;
     private final float minZoom = 0.5f;
-    private final float maxZoom = 2.0f;
+    private final float maxZoom = 2.5f;
         // Коэффициент чувствительности перемещения (1.0f — без изменений, 0.5f — в два раза медленнее)
-    private static final float PAN_SENSITIVITY = 0.3f;
+    private static final float PAN_SENSITIVITY = 0.2f;
 
 
     // Вектор текущей скорости инерционного движения и коэффициент затухания (трения)
